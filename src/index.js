4ec18ef703d594b76cbfef4fedcaf2b009e190d9
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-04-26 06:38:25 -0300

    created post account route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const { v4 } = require('uuid');
 const app = express();
 
 app.use(express.json());
+
+const customers = [];
+
+app.post('/account', (request, response) => {
+    console.log(request.body);
+
+    const { cpf, name } = request.body;
+    const uuid = v4();
+
+    customers.push({
+        cpf,
+        name,
+        uuid,
+        statament: []
+    });
+
+    return response.status(201).send(`O cliente ${cpf} foi cadastrado com sucesso`);
+});
 
 app.listen(3333);
