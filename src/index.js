b5c0f862d705af96e9cceea14734ee96f7bbf9e3
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-04-26 18:44:10 -0300

    add statement route

--- src/index.js
+++ src/index.js
@@ -24,4 +24,12 @@ app.post('/account', (request, response) => {
     return response.status(201).send(`O cliente com o ${cpf} foi cadastrado com sucesso`);
 });
 
+app.get('/statement/:cpf', (request, response) => {
+    const { cpf } = request.params;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    return response.json(customer.statament);
+});
+
 app.listen(3333);
